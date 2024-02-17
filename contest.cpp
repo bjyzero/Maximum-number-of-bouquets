575ff62b10e58ccf1eeb96571954de3559809a92
bjyzero <151053262+bjyzero@users.noreply.github.com>
2024-02-17 20:25:50 +0800

    Add files via upload

---
+++ contest.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+#define inf 1'000'000'000
+
+int main(void)
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (auto &x : arr)
+        cin >> x;
+
+    ll l = 0, r = (ll)inf * n, ret = 0;
+    while (l <= r)
+    {
+        ll mid = (l + r) >> 1;
+        ll cnt = 0;
+
+        for (const auto &x : arr)
+        {
+            cnt += min((ll)x, mid);
+        }
+
+        if (cnt / k >= mid)
+        {
+            ret = mid;
+            l = mid + 1;
+        }
+        else
+            r = mid - 1;
+    }
+
+    cout << ret << '\n';
+
+    return 0;
+}
